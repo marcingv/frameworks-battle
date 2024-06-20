e1a73b4c54ce7b8f72932c1414003e1aa6fd39f1
Marcin Gawski <marcin.gawski@gmail.com>
2024-06-20 07:54:13 +0200

    Player name edit form.

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,21 +1,27 @@
 import {useState} from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({name, symbol}) {
+  const [playerName, setPlayerName] = useState(name);
   const [isEditing, setIsEditing] = useState(false);
-
-  function handleBtnClick() {
-    setIsEditing((prevState) => !prevState);
-  }
 
   let buttonLabel = 'Edit';
   if (isEditing) {
     buttonLabel = 'Save';
   }
 
+  function handleBtnClick() {
+    setIsEditing((prevState) => !prevState);
+  }
+
+  function handlePlayerNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
   return (
     <li>
       <span className="player">
-        {isEditing ? <input type="text" required value={name} /> : <span className="player-name">{name}</span>}
+        {isEditing ? <input type="text" required value={playerName} onChange={(e) => handlePlayerNameChange(e)}/> :
+          <span className="player-name">{playerName}</span>}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleBtnClick}>{buttonLabel}</button>
