689b187408226ce1c3274b2eb176358aa58647bf
Marcin Gawski <marcin.gawski@gmail.com>
2024-06-20 09:09:36 +0200

    Switching between player rounds.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import Player from "./components/Player/Player.jsx";
 import GameBoard from "./components/GameBoard/GameBoard.jsx";
+import {useState} from "react";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer((prevActivePlayer) => prevActivePlayer === 'X' ? 'O' : 'X');
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player name="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player name="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
 
-        <GameBoard />
+        <GameBoard activePlayerSymbol={activePlayer} onSelectSquare={handleSelectSquare} />
       </div>
     </main>
   );
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -6,16 +6,25 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ activePlayerSymbol, onSelectSquare }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIdx, colIdx) {
+    if (gameBoard[rowIdx][colIdx]) {
+      // This cell is already marked by user
+      return;
+    }
+
     setGameBoard((prevState) => {
       const updatedGameBoard = [...prevState.map((oneRow) => [...oneRow])];
-      updatedGameBoard[rowIdx][colIdx] = 'X';
+      updatedGameBoard[rowIdx][colIdx] = activePlayerSymbol;
 
       return updatedGameBoard;
     });
+
+    if (onSelectSquare) {
+      onSelectSquare();
+    }
   }
 
   return (
