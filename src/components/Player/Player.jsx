76f4b7a268bb34c27e0bcbae4282d607a8c9f881
Marcin Gawski <marcin.gawski@gmail.com>
2024-06-20 07:34:31 +0200

    Player name editing UI skeleton

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,11 +1,24 @@
+import {useState} from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleBtnClick() {
+    setIsEditing((prevState) => !prevState);
+  }
+
+  let buttonLabel = 'Edit';
+  if (isEditing) {
+    buttonLabel = 'Save';
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? <input type="text" required value={name} /> : <span className="player-name">{name}</span>}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleBtnClick}>{buttonLabel}</button>
     </li>
   );
 }
