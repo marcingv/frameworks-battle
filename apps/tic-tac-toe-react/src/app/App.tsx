355f62305b18b03c30094d05b260aa5507b54aa0
Marcin Gawski <marcin.gawski@gmail.com>
2024-06-21 09:02:55 +0200

    Moved game logic functions to domain library.

--- apps/tic-tac-toe-react/src/app/App.tsx
+++ apps/tic-tac-toe-react/src/app/App.tsx
@@ -4,12 +4,13 @@ import { useState } from "react";
 import Log from "./components/Log/Log";
 import GameOver from "./components/GameOver/GameOver";
 import {
+  deriveActivePlayer,
   GameBoardGrid,
   GameTurn,
-  INITIAL_GAME_BOARD,
+  getWinner,
   INITIAL_PLAYERS,
-  PlayerSymbol,
-  WINNING_COMBINATIONS
+  initializeNewBoard,
+  PlayerSymbol
 } from "@gv-tic-tac-toe/domain";
 
 
@@ -21,42 +22,8 @@ export function App() {
   const winner: PlayerSymbol | undefined = getWinner(gameBoard);
   const isDraw: boolean = gameTurns.length === 9 && !winner;
 
-
-  function getWinner(gameBoard: GameBoardGrid): PlayerSymbol | undefined {
-    let winner = undefined;
-
-    for (const combination of WINNING_COMBINATIONS) {
-      const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
-      const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
-      const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
-
-      if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
-        winner = firstSquareSymbol;
-      }
-    }
-
-    return winner;
-  }
-
-  function initializeNewBoard(gameTurns: GameTurn[]): GameBoardGrid {
-    const board = [...INITIAL_GAME_BOARD.map((row) => [...row])];
-    gameTurns.forEach((oneTurn) => {
-      board[oneTurn.square.row][oneTurn.square.col] = oneTurn.player;
-    });
-
-    return board;
-  }
-
   function handleRematch(): void {
     setGameTurns([]);
-  }
-
-  function deriveActivePlayer(gameTurns: GameTurn[]): PlayerSymbol {
-    if (!gameTurns || !gameTurns.length || gameTurns[0].player === PlayerSymbol.O) {
-      return PlayerSymbol.X;
-    }
-
-    return PlayerSymbol.O;
   }
 
   function handleSelectSquare(rowIdx: number, colIdx: number): void {
---
+++ libs/shared/domain/src/lib/utils/game-logic.utils.ts
@@ -0,0 +1,35 @@
+import { GameBoardGrid, GameTurn, PlayerSymbol } from "../types";
+import { INITIAL_GAME_BOARD, WINNING_COMBINATIONS } from "../data";
+
+export function initializeNewBoard(gameTurns: GameTurn[]): GameBoardGrid {
+  const board = [...INITIAL_GAME_BOARD.map((row) => [...row])];
+  gameTurns.forEach((oneTurn) => {
+    board[oneTurn.square.row][oneTurn.square.col] = oneTurn.player;
+  });
+
+  return board;
+}
+
+export function deriveActivePlayer(gameTurns: GameTurn[]): PlayerSymbol {
+  if (!gameTurns || !gameTurns.length || gameTurns[0].player === PlayerSymbol.O) {
+    return PlayerSymbol.X;
+  }
+
+  return PlayerSymbol.O;
+}
+
+export function getWinner(gameBoard: GameBoardGrid): PlayerSymbol | undefined {
+  let winner = undefined;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+  return winner;
+}
