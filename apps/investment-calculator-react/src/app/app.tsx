acc5ff64750fd4e71565e0dabcd02c6296591149
Marcin Gawski <marcin.gawski@gmail.com>
2024-06-24 13:39:25 +0200

    [React] Generated new application: Investment Calculator.

---
+++ apps/investment-calculator-react/src/app/app.tsx
@@ -0,0 +1,13 @@
+import './app.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="investment-calculator-react" />
+    </div>
+  );
+}
+
+export default App;
