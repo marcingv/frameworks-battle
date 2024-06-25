415f2194b5f6543bc9c6c1d5a0fde9e09d235d1c
Marcin Gawski <marcin.gawski@gmail.com>
2024-06-25 09:05:41 +0200

    [React Styling] Generated new app.

---
+++ apps/styling-react/src/app/app.tsx
@@ -0,0 +1,14 @@
+import './app.css';
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        <span> Hello there, </span>
+        Welcome styling-react 👋
+      </h1>
+    </div>
+  );
+}
+
+export default App;
