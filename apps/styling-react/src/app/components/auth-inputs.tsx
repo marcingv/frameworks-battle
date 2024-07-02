352c411257f0929e6d58a30b536ef6f450d64b1b
Marcin Gawski <marcin.gawski@gmail.com>
2024-07-02 09:06:09 +0200

    [React Styling] Reusable styled components.

--- apps/styling-react/src/app/components/auth-inputs.tsx
+++ apps/styling-react/src/app/components/auth-inputs.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import Button from './button';
+import TextButton from './text-button';
+import LabeledInput from './labeled-input';
 
 const StyledAuthInputs = styled.div`
   width: 100%;
@@ -19,54 +22,10 @@ const ControlContainer = styled.div`
   margin-bottom: 1.5rem;
 `;
 
-const Label = styled.label<{ $invalid?: boolean }>`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $invalid }) => ($invalid ? '#f87171' : '#6b7280')};
-`;
-
-const Input = styled.input<{ $invalid?: boolean }>`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-  color: ${({ $invalid }) => ($invalid ? '#f73f3f' : '#374151')};
-  border: 1px solid ${({ $invalid }) => ($invalid ? '#ef4444' : 'transparent')};
-  background-color: ${({ $invalid }) => ($invalid ? '#fed2d2' : '#d1d5db')};
-`;
-
 const Actions = styled.div`
   display: flex;
   justify-content: flex-end;
   gap: 1rem;
-`;
-
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;
-  }
-`;
-
-const TextButton = styled.button`
-  color: #f0b322;
-  border: none;
-
-  &:hover {
-    color: #f0920e;
-  }
 `;
 
 export default function AuthInputs() {
@@ -92,24 +51,19 @@ export default function AuthInputs() {
   return (
     <StyledAuthInputs>
       <ControlContainer>
-        <p>
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
-            type="email"
-            $invalid={emailNotValid}
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
-            type="password"
-            $invalid={passwordNotValid}
-            onChange={(event) =>
-              handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
+        <LabeledInput
+          label="Email"
+          invalid={emailNotValid}
+          onChange={(event) => handleInputChange('email', event.target.value)}
+        />
+
+        <LabeledInput
+          label="Password"
+          invalid={passwordNotValid}
+          onChange={(event) =>
+            handleInputChange('password', event.target.value)
+          }
+        />
       </ControlContainer>
       <Actions>
         <TextButton type="button">Create a new account</TextButton>
