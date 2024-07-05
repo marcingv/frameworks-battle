22ca4d563cfd9dc8c8e6e7761fe728b98264ed26
Marcin Gawski <marcin.gawski@gmail.com>
2024-07-05 12:34:34 +0200

    [MFE] Serving "Styling Angular" remote app from host.

--- apps/mfe-host/src/app/app.routes.ts
+++ apps/mfe-host/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
+import { loadRemoteModule } from '@nx/angular/mf';
 
 export const appRoutes: Route[] = [
   {
     path: '',
     component: NxWelcomeComponent,
+  },
+  {
+    path: 'styling-angular',
+    loadChildren: () =>
+      loadRemoteModule('styling-angular', './Routes').then(
+        (m) => m.remoteRoutes
+      ),
   },
 ];
