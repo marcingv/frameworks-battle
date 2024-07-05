208de73f52152de6e3f4ef4f0cfbcf4370201089
Marcin Gawski <marcin.gawski@gmail.com>
2024-07-05 13:12:18 +0200

    [MFE] Sample react remote app.

---
+++ apps/mf-1-react/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'mf-1-react',
+  preset: '../../jest.preset.cjs',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/mf-1-react',
+};
