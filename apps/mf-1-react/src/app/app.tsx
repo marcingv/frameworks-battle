25e695235ab76e7ca01110eea7aa15e91d777fe3
Marcin Gawski <marcin.gawski@gmail.com>
2024-07-05 17:23:18 +0200

    [MFE] Loading React web component as micro frontend.

--- apps/mf-1-react/src/app/app.tsx
+++ apps/mf-1-react/src/app/app.tsx
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
 import NxWelcome from './nx-welcome';
+import { createRoot } from 'react-dom/client';
 
 export function App() {
   return (
@@ -11,4 +10,19 @@ export function App() {
   );
 }
 
+class Mf1ReactElement extends HTMLElement {
+  public connectedCallback(): void {
+    console.warn('Mf1ReactElement is connected: boostrap.tsx');
+
+    // ReactDOM.render(<App />, this);
+    const root = createRoot(this);
+    root.render(<App />);
+  }
+
+  public disconnectedCallback(): void {
+    console.warn('Mf1ReactElement is disconnected: boostrap.tsx');
+  }
+}
+
+customElements.define('mf1-react-element', Mf1ReactElement);
 export default App;
--- apps/mfe-host/src/app/app.routes.ts
+++ apps/mfe-host/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { Route } from '@angular/router';
 import { loadRemoteModule } from '@nx/angular/mf';
+import {
+  WebComponentWrapper,
+  WebComponentWrapperOptions,
+} from '@angular-architects/module-federation-tools';
 
 export const appRoutes: Route[] = [
   {
@@ -13,5 +17,15 @@ export const appRoutes: Route[] = [
       loadRemoteModule('styling-angular', './Routes').then(
         (m) => m.remoteRoutes
       ),
+  },
+  {
+    path: 'mf-1-react',
+    component: WebComponentWrapper,
+    data: {
+      type: 'module',
+      remoteEntry: 'http://localhost:4303/remoteEntry.js',
+      exposedModule: './Module',
+      elementName: 'mf1-react-element',
+    } satisfies WebComponentWrapperOptions,
   },
 ];
