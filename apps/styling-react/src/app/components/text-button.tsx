15f751ec837931f83d5853db37d32f78b7d824b5
Marcin Gawski <marcin.gawski@gmail.com>
2024-07-09 09:17:43 +0200

    [MFE] Styles encapsulation for micro-frontends remotes.

--- apps/styling-react/src/app/components/text-button.tsx
+++ apps/styling-react/src/app/components/text-button.tsx
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
 const TextButton = styled.button`
+  cursor: pointer;
+  background: none;
+  line-height: inherit;
   color: #f0b322;
   border: none;
 
   &:hover {
     color: #f0920e;
+    outline: none;
   }
 `;
 
