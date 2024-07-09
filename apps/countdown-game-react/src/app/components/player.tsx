01991d457f639ef34d043b9a52d8d6b95188a4e6
Marcin Gawski <marcin.gawski@gmail.com>
2024-07-09 18:11:00 +0200

    [React] [Countdown Game] Managing player name using refs.

--- apps/countdown-game-react/src/app/components/player.tsx
+++ apps/countdown-game-react/src/app/components/player.tsx
@@ -1,10 +1,22 @@
+import { useRef, useState } from 'react';
+
 export default function Player() {
+  const playerNameRef = useRef<HTMLInputElement>(null);
+  const [playerName, setPlayerName] = useState('');
+
+  function handleSubmit(): void {
+    setPlayerName(playerNameRef.current?.value ?? '');
+    if (playerNameRef.current) {
+      playerNameRef.current.value = '';
+    }
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ? playerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerNameRef} />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
