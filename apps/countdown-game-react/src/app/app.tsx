b66c62020afc59822799ee24975b07a63fc7bd55
Marcin Gawski <marcin.gawski@gmail.com>
2024-07-09 18:22:01 +0200

    [React] [Countdown Game] Timer challenge component & set of challenges.

--- apps/countdown-game-react/src/app/app.tsx
+++ apps/countdown-game-react/src/app/app.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import Player from './components/player';
 import { AppContainer } from './app-container';
+import TimerChallenge from './components/timer-challenge';
 
 export function App() {
   return (
@@ -13,8 +14,15 @@ export function App() {
           </h1>
           <p>Stop the timer once you estimate that time is (almost) up</p>
         </header>
+
         <Player />
-        <div id="challenges"></div>
+
+        <div id="challenges">
+          <TimerChallenge title="Easy" targetTime={1} />
+          <TimerChallenge title="Not easy" targetTime={5} />
+          <TimerChallenge title="Getting tough" targetTime={10} />
+          <TimerChallenge title="Pros only" targetTime={15} />
+        </div>
       </div>
     </AppContainer>
   );
---
+++ apps/countdown-game-react/src/app/components/timer-challenge.tsx
@@ -0,0 +1,23 @@
+export default function TimerChallenge({
+  title,
+  targetTime,
+}: {
+  title: string;
+  targetTime: number;
+}) {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? 's' : ''}
+      </p>
+
+      <p>
+        <button>Start Challenge</button>
+      </p>
+
+      <p className="">Time is running... / Timer inactive</p>
+    </section>
+  );
+}
