5f8ffecef83e05c5ebe2d09977353518420ffe68
Marcin Gawski <marcin.gawski@gmail.com>
2024-07-09 13:40:41 +0200

    [React] [Countdown Game] Federated app.

--- apps/countdown-game-react/src/app/app.tsx
+++ apps/countdown-game-react/src/app/app.tsx
@@ -1,14 +1,27 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import { createRoot } from 'react-dom/client';
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="countdown-game-react" />
-    </div>
-  );
+  return <div>HELLO FROM COUNTDOWN GAME :) :) :)</div>;
 }
+
+class CountdownGameReactAppElement extends HTMLElement {
+  public connectedCallback(): void {
+    console.warn('CountdownGameReactAppElement is connected: boostrap.tsx');
+
+    // ReactDOM.render(<App />, this);
+    const root = createRoot(this);
+    root.render(<App />);
+  }
+
+  public disconnectedCallback(): void {
+    console.warn('CountdownGameReactAppElement is disconnected: boostrap.tsx');
+  }
+}
+
+customElements.define(
+  'countdown-game-react-app-element',
+  CountdownGameReactAppElement
+);
 
 export default App;
