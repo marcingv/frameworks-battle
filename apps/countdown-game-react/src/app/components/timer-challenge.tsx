5e3a4d3b582248ba490cf65bb51a110f957cc4c9
Marcin Gawski <marcin.gawski@gmail.com>
2024-07-10 12:57:46 +0200

    [React] [Countdown Game] Starting & stopping challenge timer user refs.

--- apps/countdown-game-react/src/app/components/timer-challenge.tsx
+++ apps/countdown-game-react/src/app/components/timer-challenge.tsx
@@ -1,3 +1,5 @@
+import { useRef, useState } from 'react';
+
 export default function TimerChallenge({
   title,
   targetTime,
@@ -5,19 +7,47 @@ export default function TimerChallenge({
   title: string;
   targetTime: number;
 }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  const timer = useRef<NodeJS.Timeout | null>(null);
+
+  function handleStart(): void {
+    setTimerStarted(true);
+    setTimerExpired(false);
+
+    timer.current = setTimeout(() => {
+      setTimerStarted(false);
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
+
+  function handleStop(): void {
+    clearTimeout(timer.current ?? undefined);
+
+    setTimerStarted(false);
+    setTimerExpired(false);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+
+      {timerExpired && <p>You lost</p>}
 
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
 
       <p>
-        <button>Start Challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? 'Stop' : 'Start'} Challenge
+        </button>
       </p>
 
-      <p className="">Time is running... / Timer inactive</p>
+      <p className={timerStarted ? 'active' : undefined}>
+        {timerStarted ? 'Time is running...' : 'Timer inactive'}
+      </p>
     </section>
   );
 }
