fe54138c74ebf410574614636e1f4a1d0f702a9e
Marcin Gawski <marcin.gawski@gmail.com>
2024-07-16 10:43:50 +0200

    [Angular] [Countdown Game] Challenge component implementation.

--- apps/countdown-game-angular/src/app/timer-challenge/timer-challenge.component.ts
+++ apps/countdown-game-angular/src/app/timer-challenge/timer-challenge.component.ts
@@ -1,9 +1,12 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   HostBinding,
   input,
   InputSignal,
+  Signal,
+  signal,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -18,8 +21,61 @@ import { CommonModule } from '@angular/common';
 export class TimerChallengeComponent {
   public title: InputSignal<string> = input.required<string>();
   public targetTimeSecs: InputSignal<number> = input.required<number>();
+  public targetTimeMillis: Signal<number> = computed(
+    () => this.targetTimeSecs() * 1000
+  );
 
-  @HostBinding('class') protected get cssClass(): string {
+  protected timerStarted = signal<boolean>(false);
+
+  private startTime = signal<number | undefined>(undefined);
+  private stopTime = signal<number | undefined>(undefined);
+  private durationTimeMillis = computed(() => {
+    const start = this.startTime();
+    const stop = this.stopTime();
+
+    if (!start || !stop) {
+      return undefined;
+    }
+
+    return stop - start;
+  });
+  private remainingTimeMillis = computed(() => {
+    const duration = this.durationTimeMillis();
+    if (!duration) {
+      return undefined;
+    }
+
+    return this.targetTimeMillis() - duration;
+  });
+
+  private timer?: number;
+
+  @HostBinding('class')
+  protected get cssClass(): string {
     return 'challenge';
+  }
+
+  protected startTimer(): void {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+
+    this.timerStarted.set(true);
+    this.startTime.set(Date.now());
+    this.stopTime.set(undefined);
+
+    this.timer = setTimeout(() => {
+      this.stopTimer();
+    }, this.targetTimeMillis());
+  }
+
+  protected stopTimer(): void {
+    if (this.timer) {
+      clearTimeout(this.timer);
+      this.timer = undefined;
+    }
+
+    this.timerStarted.set(false);
+    this.stopTime.set(Date.now());
   }
 }
