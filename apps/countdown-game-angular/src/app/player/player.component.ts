0d0bfe190501d47ac76da288904545382fd7e695
Marcin Gawski <marcin.gawski@gmail.com>
2024-07-16 10:27:59 +0200

    [Angular] [Countdown Game] Player component implementation.

--- apps/countdown-game-angular/src/app/player/player.component.ts
+++ apps/countdown-game-angular/src/app/player/player.component.ts
@@ -1,14 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './player.component.html',
   styleUrl: './player.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlayerComponent {
-  protected playerName?: string;
+  protected playerNameCtrl: FormControl<string | null> =
+    new FormControl<string>('');
+
+  protected playerName?: string | null;
 }
